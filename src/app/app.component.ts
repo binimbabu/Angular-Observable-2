d2d12a141489cc8dd24828fde8e9154644e1d0f3
Bini M Babu <binibabu22@gmail.com>
2024-05-24 10:28:23 +0530

    Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'observable';
+export class AppComponent implements OnInit{
+  observer:any;
+  ngOnInit(): void {
+    this.observer = new Observable((subscriber) => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+     
+    console.log('just before subscribe');
+    this.observer.subscribe({
+      next(x:any) {
+        console.log('got value ' + x);
+      },
+      error(err:Error) {
+        console.error('something wrong occurred: ' + err);
+      },
+      complete() {
+        console.log('done');
+      },
+    });
+    console.log('just after subscribe');
+  }
+ 
+
+  
 }
